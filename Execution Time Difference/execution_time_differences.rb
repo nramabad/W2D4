8977af875e2e8838bda9b6cffe52b8938de6c067
nramabad <nramabad@gmail.com>
2018-11-01 15:56:21 -0700

    Add phase 2 of #sub_sum

--- Execution Time Difference/execution_time_differences.rb
+++ Execution Time Difference/execution_time_differences.rb
@@ -7,7 +7,53 @@
 #   return list.first
 # end
 
-def my_min(list)
+# PHASE II: O(n)
+def my_min(list) 
+  lowest = nil
+
+  list.each_index do |i|
+    lowest = list[i] if lowest.nil? || list[i] < lowest
+  end
+  lowest
+end
+
+# PHASE I: O(n^3)
+# def sub_sum(list)
+#   results = []
+#   list.each_index do |j|
+#     list[0..j].each_index do |i|
+#       results << list[i..j]
+#     end
+#   end
+#   max = nil
+#   results.each do |arr| 
+#     sum = arr.reduce(:+) 
+#     max = sum if max.nil? || sum > max   
+#   end
+#   max
+# end
+
+# PHASE II:
+def sub_sum(list)
+  sums = []
+  low = -1
+  high_subsum = nil
+  lowest = nil
   
   
-end 
+  list.each_index do |j|
+    sums << (sums.empty? || j == low + 1 ? list[j] : sums.last + list[j] )
+
+    if sums.last < 0
+      low = j
+    end
+    
+    lowest = j if lowest.nil? || sums[lowest] > sums[low]
+    high_subsum = j if high_subsum.nil? || sums.last > sums[high_subsum]
+
+  end
+  list[lowest + 1..high_subsum].reduce(:+)
+end
+
+list = [2, 3, -6, 7, -6, 7]
+p sub_sum(list)
