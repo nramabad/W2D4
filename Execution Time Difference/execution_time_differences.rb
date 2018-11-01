9a87dd1ab7485ba3fffef3cc7b3d4c4b9f188639
nramabad <nramabad@gmail.com>
2018-11-01 13:55:44 -0700

    Add phase I to execution_time_differences.rb

---
+++ Execution Time Difference/execution_time_differences.rb
@@ -0,0 +1,13 @@
+
+# PHASE I: O(n^2)
+# def my_min(list)
+#   list.each_index do |i|
+#     return list[i] if list.none? { |el| el < list[i] }
+#   end
+#   return list.first
+# end
+
+def my_min(list)
+  
+  
+end 
